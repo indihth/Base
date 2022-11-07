7d418c9359b5a17eb42d178490b74336dd22459e
India.h <indihth@yahoo.co.uk>
2022-11-07 15:47:18 +0000

    Route to tvshows on login

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -17,7 +17,9 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = '/dashboard';
+    
+    // Directs to tvshows on login
+    public const HOME = '/tvshows';
 
     /**
      * Define your route model bindings, pattern filters, and other route configuration.
