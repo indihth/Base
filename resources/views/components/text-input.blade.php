30ea0d2239bcdeac386578cc0e6fbf2a06075084
India.h <indihth@yahoo.co.uk>
2022-11-10 16:40:51 +0000

    wrapped in div so elements would stack

--- resources/views/components/text-input.blade.php
+++ resources/views/components/text-input.blade.php
@@ -1,10 +1,17 @@
 @props(['disabled' => false, 'field' => ''])
 
-<input {{ $disabled ? 'disabled' : '' }} {!! $attributes->merge(['class' => 'rounded-md shadow-sm border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50']) !!}>
+{{-- wrapped in a div so they displayed blocks --}}
+<div>
+    <input {{ $disabled ? 'disabled' : '' }} {!! $attributes->merge([
+        'class' =>
+            'rounded-md shadow-sm border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 mt-6 ',
+    ]) !!}>
 
 
-{{-- Displaying error messages --}}
-{{-- replace 'title' with the $field variable, can't hardcode --}}
-@error($field)
-    <div class="text-red-600 text-sm"> {{ $message }}</div> 
-@enderror
+    {{-- Displaying error messages --}}
+    {{-- replace 'title' with the $field variable, can't hardcode --}}
+    @error($field)
+        <div class="text-red-600 text-sm"> {{ $message }}</div>
+    @enderror
+
+</div>
