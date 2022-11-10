4095b1d5d3e4f5fb4d5fb2070720a92fb1e7a827
India.h <indihth@yahoo.co.uk>
2022-11-10 15:30:54 +0000

    store() (validate and create) added and show()

--- app/Http/Controllers/tvshowController.php
+++ app/Http/Controllers/tvshowController.php
@@ -21,13 +21,13 @@ class tvshowController extends Controller
         // display only notes for logged in user
         // latest, sorted by the 'update_at' column
         // $notes = Note::where('user_id', Auth::id())->latest('updated_at')->get();
-        
+
         ///////////////////////////////////////
         // paginate added to display only 'x' amount of notes per page
         // paginate() take arguement, num of notes to be displayed
 
         $tvshows =  Tvshow::where('user_id', Auth::id())->latest('updated_at')->paginate(5);
-        
+
         // shows the index view and displays the tv shows from the above variable
         return view('tvshows.index')->with('tvshows', $tvshows);
 
@@ -54,18 +54,29 @@ class tvshowController extends Controller
      */
     public function store(Request $request)
     {
-          // Validates the fields are filled, title max 120 char
-          $request->validate([
-            'title'=> 'required|max:120',
-            'description'=> 'required'
+        // Validates the fields are filled, title max 120 char
+        $request->validate([
+            'title' => 'required|max:120',
+            'description' => 'required',
+            'release_date' => 'required|date',
+            'director' => 'required|max:120',
+
+            // if 'numeric' is not included the input values will be treated as characters
+            // https://www.youtube.com/watch?v=SY375k_BFYU
+            'rating' => 'required|numeric|min:1|max:5',
+            'difficulty' => 'required|numeric|min:1|max:10'
         ]);
 
-        // Creates note, passing the user_id
+        // Creates and saves note, passing the user_id
         Tvshow::create([
-            // 'uuid' => Str::uuid(),
+            'uuid' => Str::uuid(),
             'user_id' => Auth::id(),
             'title' => $request->title,
-            'description' => $request->description
+            'description' => $request->description,
+            'release_date' => $request->release_date,
+            'director' => $request->director,
+            'rating' => $request->rating,
+            'difficulty' => $request->difficulty,
         ]);
 
         // Return to the notes page after note is added
@@ -78,9 +89,30 @@ class tvshowController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
+    public function show(Tvshow $tvshow)
     {
-        //
+        /////////////////////////////
+        // Route Model Binding
+        // Can replace show($uuid) and inject Tvshow directly instead of querying.
+        // Alt code: 
+        // $tvshow = Tvshow::where('uuid', $uuid)->where('user_id', Auth::id())->firstOrFail();
+        /////////////////////////////
+
+
+        // if($tvshows->user_id != Auth::id()) {
+        //     //403 error forbidden
+        //     return abort(403);
+        // }
+
+        /////////////////////////////
+        // Auth::id() needed to only show authorised users tvshow.
+        // Otherwise user could put any tvshow id in url and access it.
+        // FURTHER READING - laravel Gates and Policies
+        // firstOrFail displays a 404 error if the first tvshow is unavailable.
+        /////////////////////////////
+
+        // Return the tvshow view page with variable 'tvshow'
+        return view('tvshows.show')->with('tvshow', $tvshow);
     }
 
     /**
