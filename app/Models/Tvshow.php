d4d7f428853226b5ec123f5d60fb2e681db4a758
India.h <indihth@yahoo.co.uk>
2022-11-10 15:32:55 +0000

    $guarded array added to allow mass assignment, changed default Routekey to uuid

--- app/Models/Tvshow.php
+++ app/Models/Tvshow.php
@@ -9,9 +9,26 @@ class Tvshow extends Model
 {
     use HasFactory;
 
-    // changes id to uuid in URL
-    // public function getRouteKeyName()
-    // {
-    //     return 'uuid';
-    // }
+    // Specify the fields that CAN NOT be mass assigned
+    // by using the $guarded variable
+    // an empty array allows for all fields to be mass assigned
+    protected $guarded = [];
+
+
+    ////////////////////////////////
+    // Specify field that CAN be mass assigned
+    // with the $ fillable variable
+    //
+    // protected $fillable = [];
+    ////////////////////////////////
+
+    ////////////////////////////////
+    // Because I've passed Tvshow directly into show() in tvshowController
+    // it uses tvshow->id by default, 
+    // this changes the default to use tvshow->uuid
+    ////////////////////////////////
+    public function getRouteKeyName()
+    {
+        return 'uuid';
+    }
 }
