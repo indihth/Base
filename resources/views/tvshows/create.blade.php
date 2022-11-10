a3de06351d647cc168bfc76ca6ad095d4c8ca336
India.h <indihth@yahoo.co.uk>
2022-11-10 15:33:31 +0000

    added additional fields to create form

--- resources/views/tvshows/create.blade.php
+++ resources/views/tvshows/create.blade.php
@@ -9,22 +9,23 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="my-6 p-6 bg-white border-b border-gray-200 shadow-sm sm:rounded-lg">
                 <form action="{{ route('tvshows.store') }}" method="post">
-                      {{-- @csrf is used to generate a hidden field with 
-                        a csrf token to validate the request is being made 
+                      {{-- @csrf is used to generate a hidden field with a csrf token to validate the request is being made 
                         by an authorised user. Prevent outside attacks--}}
                     @csrf
 
                         {{-- if field doesn't pass validation an error message will show.
-                        'field' value is used to display error in the field's component file
-                        'value' field used to retain user input after error message from other fields
-                        :colon needed to pass in a Blade value--}}
+                        'field' value is used to display error from the component file--}}
                         <x-text-input type="text" name="title" field="title" :value="@old('title')" placeholder="Title" class="w-full" autocomplete="off"></x-text-input>
-                        {{-- <x-text-input type="text" name="director" field="director" :value="@old('director')" placeholder="Director" class="w-full" autocomplete="off"></x-text-input>
-                        <x-text-input type="text" name="release_date" field="release_date" :value="@old('release_date')" placeholder="Release Date" class="w-full" autocomplete="off"></x-text-input>
-                        <x-text-input type="text" name="rating" field="rating" :value="@old('rating')" placeholder="Rating" class="w-full" autocomplete="off"></x-text-input>
-                        <x-text-input type="text" name="difficulty" field="difficulty" :value="@old('difficulty')" placeholder="Difficulty" class="w-full" autocomplete="off"></x-text-input> --}}
 
+                        {{-- 'value' is used to retain user input after error message from other fields
+                        :colon needed to pass in a Blade value --}}
                         <x-textarea name="description" rows="10" field="description" :value="@old('description')" placeholder="Start typing here..." class="w-full mt-6"></x-textarea>
+
+                        <x-text-input type="text" name="director" field="director" :value="@old('director')" placeholder="Director" class="w-full" autocomplete="off"></x-text-input>
+                        <x-text-input type="date" name="release_date" field="release_date" :value="@old('release_date')" placeholder="Release Date" class="w-full" autocomplete="off"></x-text-input>
+                        <x-text-input type="number" name="rating" field="rating" :value="@old('rating')" placeholder="Rating" class="w-full" autocomplete="off"></x-text-input>
+                        <x-text-input type="number" name="difficulty" field="difficulty" :value="@old('difficulty')" placeholder="Difficulty" class="w-full" autocomplete="off"></x-text-input>
+
       
                         <x-primary-button class="mt-6">Save</x-primary-button>
                 </form>
